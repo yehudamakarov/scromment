55665afaba21b9d9a1ed5ac03c08a4f47c69fedf
Yehuda Makarov <yehudamakarov@gmail.com>
2021-10-15 03:05:07 -0400

    multiple commentables

--- flags.go
+++ flags.go
@@ -4,7 +4,10 @@ import (
 	"flag"
 	"log"
 	"os"
+	"strings"
 )
+
+const commentableSplitChar = "|"
 
 func makeOutFile(err error, outLocation string) *os.File {
 	out, err := os.Create(outLocation)
@@ -29,10 +32,10 @@ func check(err error) {
 	}
 }
 
-func parseFLags() (string, string, string, string) {
+func parseFLags() (string, string, []string, string) {
 	fileLocationPtr := flag.String("file-location", "", "location of the sql file that needs to be handled")
 	outLocationPtr := flag.String("out-location", "", "location of the generated sql file (that will be commented appropriately)")
-	commentablePtr := flag.String("commentable", "", "text token that must be contained in an object for this tool to comment it out")
+	commentablePtr := flag.String("commentable", "", "text token(s) that must be contained in an object for this tool to comment it out. (1 token - pass a string, 2 or more tokens - pass a pipe (|) delimited string)")
 	splitterPtr := flag.String("splitter", "/****** Object:", "text token that dictates when to split objects to be commented out when containing a commentable.")
 
 	flag.Parse()
@@ -43,24 +46,39 @@ func parseFLags() (string, string, string, string) {
 	splitter := *splitterPtr
 
 	handleCliErrors(fileLocation, outLocation, commentable, splitter)
-	return fileLocation, outLocation, commentable, splitter
+
+	commentables := getTokens(commentable)
+
+	return fileLocation, outLocation, commentables, splitter
+}
+
+func getTokens(commentable string) []string {
+	return strings.Split(commentable, commentableSplitChar)
 }
 
 func handleCliErrors(fileLocation string, outLocation string, commentable string, splitter string) {
+	// if any arg is missing
 	if fileLocation == "" {
-		showErrorMessage("file-location")
+		showMissingArgMessage("file-location")
 	}
 	if outLocation == "" {
-		showErrorMessage("out-location")
+		showMissingArgMessage("out-location")
 	}
 	if commentable == "" {
-		showErrorMessage("commentable")
+		showMissingArgMessage("commentable")
 	}
 	if splitter == "" {
-		showErrorMessage("splitter")
+		showMissingArgMessage("splitter")
+	}
+	// if used a | incorrectly for commentables
+	if strings.HasPrefix(commentable, commentableSplitChar) {
+		log.Fatal("do not start with a |")
+	}
+	if strings.HasPrefix(commentable, commentableSplitChar) {
+		log.Fatal("do not end with a |")
 	}
 }
 
-func showErrorMessage(missing string) {
+func showMissingArgMessage(missing string) {
 	log.Fatalf("don't forget the parameter for: %s!", missing)
 }
--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	fileLocation, outLocation, commentable, splitter := parseFLags()
+	fileLocation, outLocation, commentables, splitter := parseFLags()
 
 	file, err := getScriptFile(fileLocation)
 	defer closeFile(file)
@@ -15,15 +15,15 @@ func main() {
 	out := makeOutFile(err, outLocation)
 	defer closeFile(out)
 
-	convertFile(file, splitter, commentable, out)
+	convertFile(file, splitter, commentables, out)
 }
 
-func convertFile(file *os.File, splitter string, commentable string, out *os.File) {
+func convertFile(file *os.File, splitter string, commentable []string, out *os.File) {
 	scanner := getFileContent(file)
 	iterateAndRewrite(scanner, splitter, commentable, out)
 }
 
-func iterateAndRewrite(scanner *bufio.Scanner, splitter string, commentable string, out *os.File) {
+func iterateAndRewrite(scanner *bufio.Scanner, splitter string, commentable []string, out *os.File) {
 	c := chunk{sb: &strings.Builder{}}
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -41,8 +41,14 @@ func iterateAndRewrite(scanner *bufio.Scanner, splitter string, commentable stri
 	c.writeChunk(out)
 }
 
-func weShouldCommentThisLine(s string, commentable string) bool {
-	containsCommentable := strings.Contains(strings.ToLower(s), strings.ToLower(commentable))
-	notAlreadyCommentedOut := !strings.HasPrefix(strings.TrimSpace(s), "--")
-	return containsCommentable && notAlreadyCommentedOut
+func weShouldCommentThisLine(s string, commentables []string) bool {
+	containsCommentable := false
+	for _, commentable := range commentables {
+		if weFoundSomething := strings.Contains(strings.ToLower(s), strings.ToLower(commentable)); weFoundSomething {
+			containsCommentable = true
+		}
+	}
+
+	thisLineNotAlreadyCommentedOut := !strings.HasPrefix(strings.TrimSpace(s), "--")
+	return containsCommentable && thisLineNotAlreadyCommentedOut
 }
